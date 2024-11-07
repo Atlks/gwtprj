31e8a7d73d35a17b28babf98a1ab0b6b36e2ccff
attil <ati@ff.xom>
2024-11-07 16:36:55 +0800

    。。。

---
+++ src/main/java/org/example/client/MyGwtApp.java
@@ -0,0 +1,23 @@
+package org.example.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+
+public class MyGwtApp implements EntryPoint {
+
+    public static void main(String[] args) {
+        System.out.println(888);
+    }
+    @Override
+    public void onModuleLoad() {
+        // 创建并显示一个简单的 UI 组件
+        Label label = new Label("Hello, GWT!");
+        Button button = new Button("Click Me");
+        button.addClickHandler(event -> button.setText("Hello, GWT!"));
+        // 将组件添加到根面板
+        RootPanel.get().add(label);
+        RootPanel.get().add(button);
+    }
+}
